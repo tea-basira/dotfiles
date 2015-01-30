f51aabcdb8e790e87b9e0a2a35bbafdeb49252af
toorsoo <tyabatyaba.koutya@gmail.com>
2015-01-30 18:51:24 +0900

    シンボリックリンク作成シェルの更新

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/work/dotfiles/.zshrc ~/.zshrc
+ln -s ~/work/dotfiles/.bashrc ~/.bashrc
+ln -s ~/work/dotfiles/.vimrc ~/.vimrc
+ln -s ~/work/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/work/dotfiles/.gitconfig ~/.gitconfig
