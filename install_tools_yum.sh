2be6efbb8acf41d1049fd47479b6375c814b8db9
toorsoo <tyabatyaba.koutya@gmail.com>
2015-09-09 10:14:16 +0900

    toolインストールshell修正

--- install_tools_yum.sh
+++ install_tools_yum.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-#:
-#以下クローンした場所によって書き換える
-#toorsoo
-linkPath='/home/toorsoo/work/dotfiles'
-ln -s $linkPath/.zshrc ~/.zshrc
-ln -s $linkPath/.bashrc ~/.bashrc
-ln -s $linkPath/.vimrc ~/.vimrc
-ln -s $linkPath/.tmux.conf ~/.tmux.conf
-ln -s $linkPath/.gitconfig ~/.gitconfig
+
+yum install zsh
+yum install tmux
